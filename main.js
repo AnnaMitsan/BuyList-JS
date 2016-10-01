bdb42ed6e3066596322b2881f300c2990287fa24
AnnaMitsan <annamitsan@gmail.com>
2016-10-01 17:49:36 +0300

    added js

---
+++ main.js
@@ -0,0 +1,137 @@
+$(function() {
+
+    var template = $(".row-template").html();
+    var $blList = $(".bl-list");
+    var $input = $(".addinput");
+    var statisticTemplate = $(".left-template").html();
+    var $statLeft = $(".bl-left");
+    var $statDone = $(".bl-done");
+    
+    function addOneItem(name) {
+        var quantity = 1;
+        var $node = $(template);
+        var $plus = $node.find(".bl-plus");
+        var $minus = $node.find(".bl-minus");
+        var $blLabel = $node.find(".bl-label");
+        var $delete = $node.find(".bl-delete");
+        var $bought = $node.find(".bl-buy");
+        var $unBought = $node.find(".bl-notbuy");
+        var $productName = $node.find(".bl-product");
+        var $changeNameInput = $node.find(".bl-change-input");
+        
+        var $statNode = $(statisticTemplate);        
+        var $statProdName = $statNode.find(".bl-left-product-name");
+        var $statProdCount = $statNode.find(".bl-left-product-count");
+        
+        $minus.isEnabled = false;
+        
+        $productName.text(name);
+        $blLabel.text(1);
+        
+        $statProdName.text(name);
+        console.log("statprod", name)
+        $statProdCount.text(1);
+
+        function disableButton(buttonName) {
+            buttonName.attr("disabled", true);
+        }
+
+        function enableButton(buttonName) {
+            buttonName.attr("disabled", false);
+        }
+
+        function buyClick() {
+            $node.addClass("state-bought");
+            $node.removeClass("state-not-bought");
+            $statNode.remove();
+            $statDone.append($statNode);
+        }
+
+        function notBuyClick() {
+            $node.removeClass("state-bought");
+            $node.addClass("state-not-bought");
+            $statNode.remove();
+            $statLeft.append($statNode);
+        }
+
+
+        $productName.click(function() {
+            $node.addClass("state-change-name");
+            $node.removeClass("state-not-change-name");
+            var $oldName = $productName.val();
+            var $newName = "";
+            $changeNameInput.text($oldName);
+            $changeNameInput.focus();
+            $changeNameInput.on("input", function() {
+                if ($(this).val().length > 0) {
+                    $newName.val($(this).val());
+                }
+            });
+            $changeNameInput.focusout(function() {
+                $node.removeClass("state-change-name");
+                $node.addClass("state-not-change-name");
+                $productName.val($newName.val());
+            });
+
+
+        });
+
+        function plusOne() {
+            quantity++;
+            if (quantity > 1) {
+                enableButton($minus);
+            }
+            $blLabel.text(quantity);
+            $statProdCount.text(quantity);
+        }
+
+        function minusOne() {
+            quantity--;
+            if (quantity === 1) {           
+                disableButton($minus);
+            }
+            $blLabel.text(quantity);
+            $statProdCount.text(quantity);
+        }
+
+        function deleteItem() {
+            $node.remove();
+            $statNode.remove();
+        }
+
+        $bought.click(buyClick);
+        $unBought.click(notBuyClick);
+        $delete.click(deleteItem);
+        $plus.click(plusOne);
+        $minus.click(minusOne);
+        disableButton($minus);        
+        
+
+        $node.addClass("state-not-bought");
+        $node.addClass("state-not-change-name");
+        
+        $blList.append($node);
+        $statLeft.append($statNode);
+    }
+
+    $(".addbutton").click(function() {
+        var text = $input.val();
+        if (text.length > 0) {
+            addOneItem(text);
+            $input.val("");
+        }
+    });
+    $(".addinput").keypress(function(e) {
+        if (e.which == 13) {
+            var text = $input.val();
+            if (text.length > 0) {
+                addOneItem(text);
+                $input.val("");
+            }
+        }
+    });
+
+    addOneItem("Сир");
+    addOneItem("Бургер");
+    addOneItem("Coca-Cola");
+});
